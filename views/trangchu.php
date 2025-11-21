c3ed633bdeca16e25c95d531cd17c11641db71dd
duonghv <hoangvietduong07052006@gmail.com>
2025-11-22 00:00:44 +0700

    Sửa conflict trangchu.php

--- views/trangchu.php
+++ views/trangchu.php
@@ -53,7 +53,6 @@
         <a href="index.php?act=listdm"><i class="fa fa-list"></i> Danh mục tour</a>
         <a href="#"><i class="fa fa-route"></i> Quản lý tour</a>
         <a href="index.php?act=listBooking"><i class="fa fa-book"></i> Quản lý booking</a>
-        <a href="#"><i class="fa fa-book"></i> Quản lý booking</a>
         <a href="index.php?act=listNCC"><i class="fa fa-handshake"></i> Quản lý nhà cung cấp</a>
         <a href="index.php?act=listNV"><i class="fa fa-users"></i> Tài khoản / HDV</a>
         <a href="#"><i class="fa fa-chart-bar"></i> Báo cáo thống kê</a>
